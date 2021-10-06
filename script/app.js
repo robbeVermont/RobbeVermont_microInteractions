0b505234cc35f0903e95c4e274a4a55bf483f774
robbeVermont <55882306+robbeVermont@users.noreply.github.com>
2021-10-06 08:43:45 +0200

    Add files via upload

---
+++ script/app.js
@@ -0,0 +1,13 @@
+function handleFloatingLabel() {
+   
+}
+
+function handlePasswordSwitcher() {
+    
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('Script loaded!');
+    handleFloatingLabel();
+    handlePasswordSwitcher();
+});
